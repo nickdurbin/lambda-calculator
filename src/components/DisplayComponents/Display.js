b5b56f0537f4a23a83a2c37115140579f602640b
ndurbin <nick.durbin@gmail.com>
2019-08-20 05:21:52 -0400

    I have the display and most of the button styled/in position.

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Display = ({ display }) => {
-  return <div className="display">{ display }</div>;
+  return <div className="display">
+  <div className='displayDigit'>{ display }</div>
+  </div>;
 };
 
 export default Display;
